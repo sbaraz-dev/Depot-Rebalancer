bacdc3d9e3b5a44b5236de80e9a9bb23666c4bde
Sam Baraz <s.baraz.berlin@gmail.com>
2020-07-31 23:57:52 +0200

    Mein erstes commit über IntelliJ

---
+++ src/com/geldbeutel/rebalancer/ETF.java
@@ -0,0 +1,7 @@
+package com.geldbeutel.rebalancer;
+
+public class ETF {
+
+
+
+}
---
+++ src/com/geldbeutel/rebalancer/Main.java
@@ -0,0 +1,12 @@
+package com.geldbeutel.rebalancer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hallo!");
+
+
+    }
+
+}
