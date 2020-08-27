2d3f2d22a7aad62d5ab452d9b534614b5a7d3d05
Sam Baraz <s.baraz.berlin@gmail.com>
2020-08-27 02:54:54 +0200

    Implementation des Scrapers in das Projekt.

---
+++ src/main/java/com/geldbeutel/rebalancer/Main.java
@@ -0,0 +1,96 @@
+package com.geldbeutel.rebalancer;
+
+import com.geldbeutel.selenium_scraper.Scraper;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Main {
+
+    public static final String BACKUP = "/home/sam/IdeaProjects/Rebalancer2.0/src/main/java/com/geldbeutel/rebalancer/savefiles/Backup.csv";
+    public static final String DATA_DIRECTORY = "/home/sam/IdeaProjects/Rebalancer2.0/src/main/java/com/geldbeutel/rebalancer/savefiles/";
+    public static final String LAST_SAVE_FILE = "/home/sam/IdeaProjects/Rebalancer2.0/src/main/java/com/geldbeutel/rebalancer/savefiles/LastSaveFile.csv";
+
+    public static final String DATE_FORMAT_NOW = "dd-MM-yyyy (HH-mm-ss)";
+
+    public static void main(String[] args) throws IOException {
+
+        Portfolio p = createPortfolio("Kommer Faktor Portfolio", loadData(false));
+        System.out.println(p.toString());
+        p.portfolioRebalance(1000);
+        System.out.println(p.toString());
+        saveData(p);
+        Scraper.main();
+
+
+    }
+
+    // --- GETDATE METHOD FOR LATER USE --- //
+    public static String getDate(){
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+        return sdf.format(cal.getTime());
+    }
+
+    public static Portfolio createPortfolio(String name, String data) {
+        Portfolio p = new Portfolio(name);
+        String[] lines = data.split("\n");
+        for (int i = 1; i < lines.length; i++) {
+            String[] entries = lines[i].split("\\s*,\\s*");
+            p.getProduktliste().add(new ETF(entries[0]));
+            p.getProduktliste().get(i - 1).setPreis(Double.parseDouble(entries[1]));
+            p.getProduktliste().get(i - 1).setDepotwert(Double.parseDouble(entries[2]));
+            p.getProduktliste().get(i - 1).setGewichtung(Integer.parseInt(entries[3]));
+        }
+        return p;
+    }
+
+
+    // --- CREATE AND LOAD THE DATA --- //
+    public static String loadData(boolean lastSave) throws IOException {
+        if (lastSave) {
+            String inputData = "";
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(LAST_SAVE_FILE));
+            BufferedReader bufferedReader2 = new BufferedReader(new FileReader(bufferedReader.readLine().trim()));
+            for (String s = bufferedReader2.readLine(); s != null ; s = bufferedReader2.readLine()) {
+                inputData += s + "\n";
+            }
+            return inputData;
+        } else {
+            String inputData = "";
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(BACKUP));
+            for (String s = bufferedReader.readLine(); s != null ; s = bufferedReader.readLine()) {
+                inputData += s + "\n";
+            }
+            return inputData;
+        }
+    }
+
+    public static void saveData(Portfolio p) throws IOException {
+
+        String[] arrayString = p.toString().split("\n");
+        String output = "";
+        for (int i = 0; i < arrayString.length - 1; i++){
+            String[] subarray = arrayString[i].split("\\s*Preis:\\s*");
+            subarray[0] = subarray[0].replace("Name:", "").trim();
+            subarray[1] = subarray[1].replaceAll("Depotwert:", " ").trim();
+            subarray[1] = subarray[1].replaceAll("Gewichtung:", " ").trim();
+            subarray[1] = subarray[1].replaceAll("\t", " ");
+            subarray[1] = subarray[1].replaceAll(" +", ",");
+            output += subarray[0] + ", " + subarray[1] + "\n";
+        }
+
+        FileWriter fileWriter = new FileWriter(String.format("%s%s%s%s", DATA_DIRECTORY, "SaveFile_", getDate(), ".csv"), false);
+        FileWriter fileWriter2 = new FileWriter(LAST_SAVE_FILE, false);
+
+        fileWriter.write("Name, Preis, Depotstand, Gewichtung\n");
+        fileWriter.write(output);
+        fileWriter.close();
+
+        fileWriter2.write(String.format("%s%s%s%s", DATA_DIRECTORY, "SaveFile_", getDate(), ".csv"));
+        fileWriter2.close();
+
+    }
+
+}
---
+++ src/main/java/com/geldbeutel/selenium_scraper/Scraper.java
@@ -0,0 +1,47 @@
+package com.geldbeutel.selenium_scraper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
+public class Scraper {
+
+    public static final String[] PATHS = {"https://de.finance.yahoo.com/quote/IS3R.DE?p=IS3R.DE&.tsrc=fin-srch&guccounter=1",
+            "https://de.finance.yahoo.com/quote/IS3Q.DE?p=IS3Q.DE&.tsrc=fin-srch",
+            "https://de.finance.yahoo.com/quote/IS3N.DE?p=IS3N.DE&.tsrc=fin-srch",
+            "https://de.finance.yahoo.com/quote/IS3S.DE?p=IS3S.DE&.tsrc=fin-srch",
+            "https://de.finance.yahoo.com/quote/IUSN.DE?p=IUSN.DE&.tsrc=fin-srch"};
+    public static final String[] NAMES = {"Momentum: ", "Quality: ", "EM IMI: ", "Value: ", "Small Cap: "};
+
+    public static void main() {
+        //webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        LinkedList<String> liste = new LinkedList<String>();
+
+        System.setProperty("webdriver.gecko.driver", "/home/sam/IdeaProjects/geckodriver");
+        WebDriver webDriver = new FirefoxDriver();
+
+        for (int i = 0; i < PATHS.length; i++) {
+            webDriver.get(String.format("%s", PATHS[i]));
+            //webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            // AUF POPUPS PRÜFEN
+            if (webDriver.findElements(By.xpath("//button[@name='agree']")).size() != 0) {
+                webDriver.findElement(By.xpath("//button[@name='agree']")).click();
+            }
+            // AUF REDIRECTIONS PRÜFEN
+            if (webDriver.findElements(By.xpath("//a[contains(text(),'here')]")).size() != 0) {
+                webDriver.findElement(By.xpath("//a[contains(text(),'here')]")).click(); // Popup für Coockies schließen
+            }
+            WebElement kursstand = webDriver.findElement(By.xpath("//span[@class='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)']"));
+            liste.add(NAMES[i] + kursstand.getText());
+        }
+
+        webDriver.close();
+        System.out.println(liste.toString());
+
+    }
+
+}
